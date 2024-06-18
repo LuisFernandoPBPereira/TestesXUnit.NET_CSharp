9597f677f60dcc647e4576e4e5eb208e5a2eb9ca
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2024-06-18 11:40:12 -0300

    test: Adicionando testes para construtor da classe OfertaViagem

---
+++ tests/JornadaMilhas.Test/TestesListaDeExercicios/MusicaConstrutor.cs
@@ -0,0 +1,39 @@
+﻿using JornadaMilhas.ListaDeExercicios;
+
+namespace JornadaMilhas.Test.TestesListaDeExercicios;
+
+public class MusicaConstrutor
+{
+    [Theory]
+    [InlineData("She's Gone")]
+    [InlineData("Scar Tissue")]
+    public void RetornaVerdadeiroQuandoNomeFoiInicializadoCorretamenteNaMusica(string nomeMusica)
+    {
+        Musica musica = new Musica(nomeMusica);
+
+        Assert.Equal(nomeMusica, musica.Nome);
+    }
+    
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public void RetornaVerdadeiroQuandoIdDaMusicaFoiInicializadoCorretamente(int idMusica)
+    {
+        Musica musica = new Musica("She's Gone") { Id = idMusica };
+
+        Assert.True(idMusica == musica.Id);
+    }
+    
+    [Theory]
+    [InlineData(1, "She's Gone")]
+    [InlineData(2, "Scar Tissue")]
+    [InlineData(3, "Stricken")]
+    public void RetornaStringDoMetodoToStringQuandoMusicaInicializadaEMetodoChamado(int idMusica, string nomeMusica)
+    {
+        Musica musica = new Musica(nomeMusica);
+        musica.Id = idMusica;
+
+        Assert.Contains($"Id: {idMusica} Nome: {nomeMusica}", musica.ToString());
+    }
+}
